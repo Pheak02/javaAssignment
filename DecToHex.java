6f60acba3d9afd571f04d0a56e03ab1c1263b572
Pheak02 <email@gmail.com>
2023-01-23 22:05:53 +0700

    DecToHex also work  without constructor

--- DecToHex.java
+++ DecToHex.java
@@ -1,7 +1,37 @@
+// import java.util.Scanner;
+
+// public class DecToHex {
+//     public DecToHex() {
+//         // create a constructor for the data structure
+//         Scanner userInput = new Scanner(System.in);
+//         int decimal, remainder;
+//         char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+//         String hexValue = "";
+
+//         System.out.println("Enter any number: ");
+//         decimal = userInput.nextInt();
+
+//         while (decimal > 0) {
+//             remainder = decimal % 16;
+//             hexValue = hex[remainder] + hexValue;
+//             decimal = decimal / 16;
+
+//         }
+//         System.out.println("Hexa value is: " + hexValue);
+//     }
+
+//     public static void main(String[] args) {
+//         DecToHex obj = new DecToHex();
+
+//     }
+// }
+
+// try not to use the constructor
 import java.util.Scanner;
 
 public class DecToHex {
-    public DecToHex() {
+
+    public static void main(String[] args) {
         // create a constructor for the data structure
         Scanner userInput = new Scanner(System.in);
         int decimal, remainder;
@@ -20,8 +50,4 @@ public class DecToHex {
         System.out.println("Hexa value is: " + hexValue);
     }
 
-    public static void main(String[] args) {
-        DecToHex obj = new DecToHex();
-
-    }
 }
