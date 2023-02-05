39502d0a253f52297f46dd98c37d9b011020b16b
Pheak02 <email@gmail.com>
2023-02-05 12:40:22 +0700

    change file name

---
+++ Ch2_Exercise7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ch2_Exercise7 {
+
+    public static void main(String[] args) {
+
+        Scanner userGuess = new Scanner(System.in);
+        int correctCount = 0;
+        for (int i = 0; i < stateCapitals.length; i++) {
+            System.out.print("What is the capital of " + stateCapitals[i][0] + "? ");
+            String s = userGuess.nextLine();
+            if (s.equalsIgnoreCase(stateCapitals[i][1])) {
+                System.out.println("Wahh! You got the correct answer, CONGRATULATIONS!");
+                correctCount++;
+            } else {
+                System.out.println("Nahh, not yet correct! The correct answer is " + stateCapitals[i][1]);
+            }
+        }
+        System.out.println("The correct count is " + correctCount);
+    }
+    // improve:
+    // 1. count the correct answer
+    // stop when quess wrong ans
+
+    private static String[][] stateCapitals = {
+            { "Cambodia", "Phnom Penh" },
+            { "Thailand", "Bangkok" },
+            { "China", "Beijing" },
+            { "Japan", "Tokyo" },
+            { "India", "Delhi" },
+            { "Malaysia", "Kuala Lumpur" }
+    };
+}
