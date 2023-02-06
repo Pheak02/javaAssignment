67090754b3d0c628bb6b6f915beb8c6aecdd7b56
Pheak02 <email@gmail.com>
2023-02-06 23:55:27 +0700

    implement correct count

--- Ch3_Exercise7.java
+++ Ch3_Exercise7.java
@@ -6,16 +6,17 @@ public class Ch3_Exercise7 {
 
         Scanner userGuess = new Scanner(System.in);
         int correctCount = 0;
-        for (int i = 0; i < stateCapitals.length; i++) {
-            i = (int) (Math.random() * stateCapitals.length);
+        for (int i = 0; i < city.length; i++) {
+            i = (int) (Math.random() * city.length);
 
-            System.out.print("What is the capital of " + stateCapitals[i][0] + "? ");
+            System.out.print("What is the capital city of " + city[i][0] + "? ");
             String s = userGuess.nextLine();
-            if (s.equalsIgnoreCase(stateCapitals[i][1])) {
+            if (s.equalsIgnoreCase(city[i][1])) {
                 System.out.println("Wahh! You got the correct answer, CONGRATULATIONS!");
                 correctCount++;
+                break;
             } else {
-                System.out.println("Nahh, not yet correct! The correct answer is " + stateCapitals[i][1]);
+                System.out.println("Nahh, not yet correct! The correct answer is " + city[i][1]);
             }
         }
         System.out.println("The correct count is " + correctCount);
@@ -24,7 +25,7 @@ public class Ch3_Exercise7 {
     // 1. count the correct answer
     // stop when quess wrong ans
 
-    private static String[][] stateCapitals = {
+    private static String[][] city = {
             { "Cambodia", "Phnom Penh" },
             { "Thailand", "Bangkok" },
             { "China", "Beijing" },
