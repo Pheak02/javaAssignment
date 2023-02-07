440f4bc88cbe230dcbb4c6194d0a596019af1ae1
Pheak02 <email@gmail.com>
2023-02-07 09:55:47 +0700

    update ex 7 ch3

--- Ch3_Exercise7.java
+++ Ch3_Exercise7.java
@@ -8,8 +8,8 @@ public class Ch3_Exercise7 {
         int correctCount = 0;
         for (int i = 0; i < city.length; i++) {
             i = (int) (Math.random() * city.length); // random 1 among the all lenght.
-
             System.out.print("What is the capital city of " + city[i][0] + "? "); // get user input & access arr memo
+
             String s = userGuess.nextLine();
             if (s.equalsIgnoreCase(city[i][1])) {
                 System.out.println("Wahh! You got the correct answer, CONGRATULATIONS!");
@@ -17,7 +17,9 @@ public class Ch3_Exercise7 {
             } else {
                 System.out.println("Nahh, not yet correct! The correct answer is " + city[i][1]);
                 break;
+
             }
+
         }
         System.out.println("The correct count is " + correctCount);
     }
@@ -25,7 +27,7 @@ public class Ch3_Exercise7 {
     // 1. count the correct answer
     // stop when quess wrong ans
 
-    private static String[][] city = {
+    public static String[][] city = {
             { "Cambodia", "Phnom Penh" },
             { "Thailand", "Bangkok" },
             { "China", "Beijing" },
