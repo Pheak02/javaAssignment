06c971296b9896eee82c02738c72972999437e39
Pheak02 <saingsopheak02@gmail.com>
2023-02-28 09:00:34 +0700

    Ch3_Ex1 updated

--- Ch3_Exercise1.java
+++ Ch3_Exercise1.java
@@ -5,6 +5,7 @@ public class Ch3_Exercise1 {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix.length; col++) {
                 int randomLetter = (int) (Math.random() * 26) + 'A';// A is the ascii code, A=65
+                // OR int randomLetter = (int) (Math.random() * 26) + '65';
                 matrix[row][col] = (char) randomLetter; // change from ascii code to character
                 System.out.print(matrix[row][col] + " ");
             }
