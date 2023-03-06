e8625bde4e6e850a37d87686ea56ae52d2322285
Pheak02 <saingsopheak02@gmail.com>
2023-03-06 17:50:31 +0700

    get some idea

---
+++ Ch4_Exercise7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// public class Ch4_Exercise7 {
+// public static void main(String[] args) {
+// System.out.print("Please enter a statement: ");
+// Scanner s1 = new Scanner(System.in);// get input from user
+// String mainString = s1.nextLine(); // input line of text from user
+// System.out.print("Enter the substring you want to remove:");
+// Scanner s2 = new Scanner(System.in);
+// String subString = s2.nextLine();
+// int findInd = s1.indexOf(s2);
+// if (findInd = -1) {
+// int len = s2 / length();
+
+// }
+
+// }
+// }
