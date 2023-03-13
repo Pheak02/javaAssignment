b211b0c2ca12beab7f7f1753e9f2e18ac27e928a
Pheak02 <saingsopheak02@gmail.com>
2023-03-13 21:28:34 +0700

    at least get some concept of ch4 ex16

---
+++ Ch4_Exerecise16.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ch4_Exerecise16 {
+    public static void main(String[] args) {
+        removeInt();
+
+    }
+
+    static void removeInt() {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array:");
+        System.out.println("Enter any integer you want:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+            if (a[i]==a[j])
+
+        }
+
+    }
+}
